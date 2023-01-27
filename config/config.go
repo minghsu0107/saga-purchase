5440e30530cf8fa77120d1912674754ccd97c489
ming.hsu <ming.hsu@appier.com>
2023-01-27 17:48:46 +0800

    fix config

--- config/config.go
+++ config/config.go
@@ -41,14 +41,13 @@ type NATSSubscriber struct {
 
 // RedisConfig is redis config type
 type RedisConfig struct {
-	Addrs              string           `yaml:"addrs" envconfig:"REDIS_ADDRS"`
-	Password           string           `yaml:"password" envconfig:"REDIS_PASSWORD"`
-	DB                 int              `yaml:"db" envconfig:"REDIS_DB"`
-	PoolSize           int              `yaml:"poolSize" envconfig:"REDIS_POOL_SIZE"`
-	MaxRetries         int              `yaml:"maxRetries" envconfig:"REDIS_MAX_RETRIES"`
-	ExpirationSeconds  int64            `yaml:"expirationSeconds" envconfig:"REDIS_EXPIRATION_SECONDS"`
-	IdleTimeoutSeconds int64            `yaml:"idleTimeoutSeconds" envconfig:"REDIS_IDLE_TIMEOUT_SECONDS"`
-	Subscriber         *RedisSubscriber `yaml:"subscriber"`
+	Addrs             string           `yaml:"addrs" envconfig:"REDIS_ADDRS"`
+	Password          string           `yaml:"password" envconfig:"REDIS_PASSWORD"`
+	DB                int              `yaml:"db" envconfig:"REDIS_DB"`
+	PoolSize          int              `yaml:"poolSize" envconfig:"REDIS_POOL_SIZE"`
+	MaxRetries        int              `yaml:"maxRetries" envconfig:"REDIS_MAX_RETRIES"`
+	ExpirationSeconds int64            `yaml:"expirationSeconds" envconfig:"REDIS_EXPIRATION_SECONDS"`
+	Subscriber        *RedisSubscriber `yaml:"subscriber"`
 }
 
 type RedisSubscriber struct {
